218e37403ca6f55575d666ba44ee950705da5a41
Neel Patel <patelneel27796@gmail.com>
2018-10-11 15:10:29 +0530

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+  
+})
+export class AppComponent {
+  title = 'Angular-Tour-of-Heroes';
+}
